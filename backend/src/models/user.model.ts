0350c67554cee07b290d3c18a63ff208202cd248
Javier Perez <javier@iul-inst.com>
2022-08-02 10:23:18 +0200

    modified backend to add basic login/register functionality, added nodemon for hot reloading

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const User = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+    quote: { type: String },
+  },
+  { collection: "user-data" }
+);
+
+const model = mongoose.model("UserData", User);
+
+export default model;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,46 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import User from "./models/user.model";
 const app = express();
-
+const port = process.env.PORT || 8080;
 app.use(cors());
+app.use(express.json());
 
-app.use('/login', (req, res) => {
-  res.send({
-    token: 'test123'
-  });
+mongoose.connect("mongodb://localhost:27017/rick-and-morty-auth");
+
+app.post("/api/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    await User.create({
+      name: name,
+      email: email,
+      password: password,
+    });
+    return res.json({ status: "ok" });
+  } catch (error) {
+    return res.json({ status: "error" });
+  }
 });
 
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({
+      email: email,
+      password: password,
+    });
+
+    if (user) {
+      return res.json({ status: "ok", user: true });
+    }
+
+    return res.json({ status: "error", user: false });
+  } catch (error) {
+    return res.json({ status: "error", description: error });
+  }
+});
+
+app.listen(port, () =>
+  console.log("API is running on http://localhost:8080/api")
+);
