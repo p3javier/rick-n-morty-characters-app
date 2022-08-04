2e77fc10f2f24a61f922a8ac3ac94636155b08fc
Javier Perez <javier@iul-inst.com>
2022-08-04 16:32:59 +0200

    modified character card to improve visual layout, also custom Hide component has been added for better responsive design

--- frontend/src/features/CharacterCard/CharacterCard.tsx
+++ frontend/src/features/CharacterCard/CharacterCard.tsx
@@ -3,22 +3,36 @@ import { Character } from "../../types/characterInterface";
 import "./characterCardStyles.css";
 import { Link } from "react-router-dom";
 import AddFavorite from "../AddFavorite/AddFavorite";
+import Hide from "../../components/Hide/Hide";
+
 const CharacterCard = (props: Character) => {
   const { name, image, url } = props;
   return (
     <div className="card character-card container">
       <div className="row">
-        <img src={image} alt={name} className="col-6" />
-        <h2 className="col-6">
-          <Link to={`/character/${url}`}>{name}</Link>
-        </h2>
-      </div>
-      <div className="row">
-        <Link className="details row col-4" to={`/character/${url}`}>
-          View details
-        </Link>
+        <img src={image} alt={name} className="col-4" />
+        <div className="col-8 parent">
+          <div className="row">
+            <h2 className="col-12">
+              <Link to={`/character/${url}`}>{name}</Link>
+            </h2>
+          </div>
+          <div className="row child">
+            <Hide md lg xl>
+              <Link className="details col-6" to={`/character/${url}`}>
+                <div className="text-title">View details</div>
+              </Link>
 
-        <AddFavorite />
+              <div className="col-6" style={{ flex: "display" }}>
+                <div className="row">
+                  <h4>Add to favorites</h4>
+                  <AddFavorite />
+                </div>
+              </div>
+            </Hide>
+            <Hide sm>NUEVO</Hide>
+          </div>
+        </div>
       </div>
     </div>
   );
