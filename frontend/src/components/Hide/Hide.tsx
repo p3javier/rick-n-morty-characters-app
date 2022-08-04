3f10f38356b6f1ca4f3d6b1e35b3cf28826ee440
p3javier <javii.e.saverroes@gmail.com>
2022-08-04 23:51:59 +0200

    finished implementation of Hide component

--- frontend/src/components/Hide/Hide.tsx
+++ frontend/src/components/Hide/Hide.tsx
@@ -8,6 +8,14 @@ interface HideProps {
   md?: boolean | true;
   lg?: boolean;
   xl?: boolean;
+  xsUp?: boolean;
+  smUp?: boolean;
+  mdUp?: boolean;
+  lgUp?: boolean;
+  xsDown?: boolean;
+  smDown?: boolean;
+  mdDown?: boolean;
+  lgDown?: boolean;
 }
 
 //type Size = "xs" | "sm" | "md" | "lg" | "xl";
@@ -19,14 +27,30 @@ const Hide = (props: HideProps) => {
   const getQuery: (size: string) => string = (size) => {
     switch (size) {
       case "xl":
-        return "(min-width: 900px)";
+        return "(min-width: 1024px)";
       case "lg":
-        return "(min-width: 768px) and (max-width: 900px)";
+        return "(min-width: 900px) and (max-width: 1024px)";
       case "md":
         return "(min-width: 768px) and (max-width: 900px)";
       case "sm":
         return "(min-width: 420px) and (max-width: 768px)";
       case "xs":
+        return "(max-width: 420px)";
+      case "lgUp":
+        return "(min-width: 900px)";
+      case "mdUp":
+        return "(min-width: 768px)";
+      case "smUp":
+        return "(min-width: 420px)";
+      case "xsUp":
+        return "(min-width: 0px)";
+      case "xlDown":
+        return "(max-width: 1024px)";
+      case "lgDown":
+        return "(max-width: 900px)";
+      case "mdDown":
+        return "(max-width: 768px)";
+      case "smDown":
         return "(max-width: 420px)";
 
       default:
@@ -36,7 +60,7 @@ const Hide = (props: HideProps) => {
 
   const getSize: () => string = () => {
     const propString = Object.keys(mediaSize)[0];
-    const validValues = ["xs", "sm", "md", "lg", "xl"];
+    const validValues = ["xs", "sm", "md", "lg", "xl", "mdUp"];
     if (validValues.includes(propString)) {
       return propString;
     }
