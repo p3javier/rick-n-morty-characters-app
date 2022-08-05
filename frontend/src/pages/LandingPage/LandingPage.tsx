25553153fc1a5080a029f48940ed0d6b96f3d574
p3javier <javii.e.saverroes@gmail.com>
2022-08-05 02:20:15 +0200

    added basic landing page

--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const LandingPage = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <div>
+      <img src="banner.jpg" />
+      <div className="container">
+        <h1>Introduction</h1>
+        <div>
+          <p>
+            This is a personal project with the main purpose of being used
+            interview.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
